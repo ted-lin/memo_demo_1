03a4706c0a866937d52a23f7ac3d58c6d634ce27
jacky ho <jackyhobingo@gmail.com>
2021-10-04 18:37:19 +0800

    Modified some button and add proto

--- app/src/main/java/com/example/memo_demo/EditorActivity.java
+++ app/src/main/java/com/example/memo_demo/EditorActivity.java
@@ -1,18 +1,34 @@
 package com.example.memo_demo;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.AttributeSet;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+
+import androidx.annotation.Nullable;
+import androidx.webkit.WebViewAssetLoader;
+
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 import jp.wasabeef.richeditor.RichEditor;
 
 public class EditorActivity extends AppCompatActivity {
     public static final String TAG = "EditorActivity";
-
     private RichEditor mEditor;
     private Button redBtn;
     private Button blackBtn;
@@ -20,8 +36,17 @@ public class EditorActivity extends AppCompatActivity {
     private Button smallBtn;
     private Button htmlBtn;
     private Button loadHtmlBtn;
+    private Button protoBtn;
+    private Button imageBtn;
     String html;
     private int mFontSize = 5;
+    private View.OnClickListener imageInsert = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            mEditor.insertImage("file://" + Environment.getExternalStorageDirectory().getAbsolutePath() + "/s1.png", "", 400, 500);
+        }
+    };
     private View.OnClickListener toRed = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -70,12 +95,26 @@ public class EditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .addPathHandler("/images/", new WebViewAssetLoader.InternalStoragePathHandler(this, getFilesDir()))
+                .build();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activit_editor);
 
         mEditor = findViewById(R.id.editor);
         mEditor.setFontSize(22);
         mEditor.setEditorHeight(200);
+        mEditor.getSettings().setAllowContentAccess(true);
+        mEditor.getSettings().setAllowFileAccess(true);
+        mEditor.getSettings().setAllowFileAccessFromFileURLs(true);
+//        mEditor.
+//        mEditor.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+//                return assetLoader.shouldInterceptRequest(request.getUrl());
+//            }
+//        });
 
         redBtn = findViewById(R.id.btnRed);
         blackBtn = findViewById(R.id.btnBlack);
@@ -83,12 +122,15 @@ public class EditorActivity extends AppCompatActivity {
         smallBtn = findViewById(R.id.btnSmall);
         htmlBtn = findViewById(R.id.btnHtml);
         loadHtmlBtn = findViewById(R.id.btnLoadHtml);
+        imageBtn = findViewById(R.id.btnImg);
         redBtn.setOnClickListener(toRed);
         blackBtn.setOnClickListener(toBlack);
         smallBtn.setOnClickListener(toSmall);
         bigBtn.setOnClickListener(toBig);
         htmlBtn.setOnClickListener(getHtml);
         loadHtmlBtn.setOnClickListener(loadHtml);
+        imageBtn.setOnClickListener(imageInsert);
+
 
     }
 }
