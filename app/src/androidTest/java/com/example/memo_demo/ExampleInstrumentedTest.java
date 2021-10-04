221155a6c087fc1131b63b912aeba00f16ced59e
jacky ho <jackyhobingo@gmail.com>
2021-10-04 18:37:19 +0800

    Add file to .gitignore for untracing

--- app/src/androidTest/java/com/example/memo_demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/memo_demo/ExampleInstrumentedTest.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import static org.junit.Assert.assertEquals;
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+//@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
