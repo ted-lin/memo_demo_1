6c2d4b95f5888900d0bbf29bd0a5ae538e847669
ted-lin <m983040031@gmail.com>
2021-10-05 02:50:49 +0800

    integrate

---
+++ app/src/main/java/com/example/wifi/SocketConfig.java
@@ -0,0 +1,6 @@
+package com.example.wifi;
+
+public class SocketConfig {
+    public static final int SOCKET_PORT = 9999;
+    public static final int END_CHAR = '$';
+}
---
+++ app/src/main/java/com/example/wifi/WifiDirectListener.java
@@ -0,0 +1,21 @@
+package com.example.wifi;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+
+import java.util.Collection;
+
+public interface WifiDirectListener extends WifiP2pManager.ChannelListener {
+    void onWifiDirectEnabled(boolean enabled);
+
+    void onPeersChanged(Collection<WifiP2pDevice> wifiP2pDeviceList);
+
+    void onConnect(WifiP2pInfo p2pInfo);
+
+    void onDisconnect(WifiP2pInfo p2pInfo);
+
+    void onSelfChanged(WifiP2pDevice p2pDevice);
+
+    void onDiscoveryChanged(int discoveryState);
+}
