8931beecc2ec5730c5b259717a33954f56db541e
jacky ho <jackyhobingo@gmail.com>
2021-10-08 12:55:51 +0800

    Update TextHelper and TextHelperUnitTest

--- app/src/main/java/com/example/memo_demo/TextHelper.java
+++ app/src/main/java/com/example/memo_demo/TextHelper.java
@@ -4,8 +4,9 @@ package com.example.memo_demo;
 public class TextHelper {
 
     public static String toPlainTxt(String html) {
-        return html.replaceAll("<br>", "\n")
+        return html.replaceAll("<[ ]*br[ ]*/?[ ]*>", "\n")
                 .replaceAll("<[^>]*[^\\\\s>][^>]*>", "")
+                .replaceAll("<[^>]*[^\\\\s>][^>]*/>", "")
                 .replaceAll("&nbsp;", " ")
                 .replaceAll("&amp;", "&")
                 .replaceAll("&lt;", "<")
