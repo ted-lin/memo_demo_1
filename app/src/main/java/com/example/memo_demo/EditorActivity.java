5a51392b12a314f613ba505294fe145348ecf716
jacky ho <jackyhobingo@gmail.com>
2021-10-22 10:31:10 +0800

    fix: forget to call super onActivityResult

--- app/src/main/java/com/example/memo_demo/EditorActivity.java
+++ app/src/main/java/com/example/memo_demo/EditorActivity.java
@@ -420,6 +420,7 @@ public class EditorActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode != RESULT_OK || data == null)
             return;
         Uri uri = data.getData();
